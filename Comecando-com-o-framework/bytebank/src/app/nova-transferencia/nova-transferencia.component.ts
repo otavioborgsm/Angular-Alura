ffad43fafc0fe26a5531aa655b0c2c6e01584825
Otávio Borges <otavio.demb@rede.ulbra.br>
2021-10-03 23:42:41 -0300

    curso finalizado

---
+++ Comecando-com-o-framework/bytebank/src/app/nova-transferencia/nova-transferencia.component.ts
@@ -0,0 +1,42 @@
+import { Router } from '@angular/router';
+import { Transferencia } from './../models/transferencia.model';
+import { TransferenciaService } from './../services/transferencia.service';
+import { HttpClient } from '@angular/common/http';
+import { Component, Output } from "@angular/core";
+import { EventEmitter } from "@angular/core";
+
+@Component({
+    selector:'app-nova-transferencia',
+    templateUrl: './nova-transferencia.component.html',
+    styleUrls: ['./nova-transferencia.component.scss']
+})
+
+export class NovaTransferenciaComponent{
+  @Output() aoTransferir = new EventEmitter<any>();
+
+  valor: number = 0;
+  destino: number = 0;
+
+  constructor(private service: TransferenciaService, private router: Router){}
+
+  transferir(){
+
+    console.log("Transferência Efetuada");
+
+    const valorEmitir: Transferencia = {valor: this.valor, destino: this.destino};
+
+    this.service.adicionar(valorEmitir).subscribe(resultado =>{
+      console.log(resultado);
+      this.limparCampos();
+      this.router.navigateByUrl('extrato');
+    },
+    error =>console.error(error)
+    );
+
+  }
+
+  limparCampos(){
+    this.valor = 0;
+    this.destino = 0;
+  }
+}
